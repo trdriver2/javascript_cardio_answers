c98d2f26f31efb558531fa88e13239c48d1e092f
trdriver2 <trdriver@talentpath.com>
2019-10-14 21:35:26 -0500

    solved all of the challenges for session 1 and 2

--- session1/index_start.js
+++ session1/index_start.js
@@ -3,16 +3,22 @@
 // ex. reverseString('hello') === 'olleh'
 
 function reverseString(str) {
-  return 'Let\'s Start'
+  let reverse = ""
+  for(i = (str.length -1); i>=0; i--)
+    reverse += str[i]  
+  return reverse
 }
-
 
 
 // CHALLENGE 2: VALIDATE A PALINDROME
 // Return true if palindrome and false if not
 // ex. isPalindrome('racecar') === 'true', isPalindrome('hello') == false
 
-function isPalindrome(str) {}
+function isPalindrome(str) {
+  if(str === reverseString(str))
+    return true;
+  return false;
+}
 
 
 
@@ -20,31 +26,91 @@ function isPalindrome(str) {}
 // Return an integer in reverse
 // ex. reverseInt(521) === 125
 
-function reverseInt(int) {}
+function reverseInt(int) {
+  temp = reverseString(int.toString())
+  let pos = true
+  if(temp[temp.length - 1] === '-')
+  {
+    pos = false
+    var arr = temp.split('')
+    arr.splice(temp.length-1, 1)
+    //temp='-' + arr.join()
+  }
+  num = parseFloat(temp,10);
+  if(!pos)
+    num = 0-num;
+  return num
+}
 
 
 
 // CHALLENGE 4: CAPITALIZE LETTERS
 // Return a string with the first letter of every word capitalized
 // ex. capitalizeLetters('i love javascript') === 'I Love Javascript'
-function capitalizeLetters(str) {}
+function capitalizeLetters(str) {
+  return(str.toUpperCase())
+}
 
 
 
 // CHALLENGE 5: MAX CHARACTER
 // Return the character that is most common in a string
 // ex. maxCharacter('javascript') == 'a'
-function maxCharacter(str) {}
+function maxCharacter(str) {
+  final = new Array
+  for(i = 0; i< str.length; i++)
+  {
+    contained = false
+    for(j=0; j<final.length && !contained; j++)
+    {
+      if(final[j][0] === str[i])
+      {
+        final[j][1]++;
+        contained=true;
+      }      
+      
+    }
+    if(!contained)
+    {
+      a = [str[i], 1]
+      final.push(a)
+    }    
+  }
+
+  let highest=['z',0];
+  console.log(final)
+  for(i=0; i<final.length; i++)
+  {
+    if(final[i][1] > highest[1])
+    {
+      highest[0] = final[i][0]
+      highest[1] = final[i][1]
+    }
+  }
+  return highest;
+}
 
 
 
 // CHALLENGE 6: FIZZBUZZ
 // Write a program that prints all the numbers from 1 to 100. For multiples of 3, instead of the number, print "Fizz", for multiples of 5 print "Buzz". For numbers which are multiples of both 3 and 5, print "FizzBuzz".
-function fizzBuzz() {}
+function fizzBuzz() {
+  for(i = 1; i<101;i++)
+  {
+    if(i%3===0 && i%5===0)
+      console.log("FizzBuzz")
+    else if(i%3===0)
+      console.log("Fizz")
+    else if(i%5===0)
+      console.log("Buzz")
+    else 
+      console.log(i);
+  }
+}
 
 
 
 // Call Function
-const output = reverseString('hello');
+const output = capitalizeLetters("aaaaaaaaakjhsdfkjkdfhjshkdfjshkfh");
 
 console.log(output);
--- session2/index_start.js
+++ session2/index_start.js
@@ -4,8 +4,41 @@
 
 function longestWord(sen) {
   // SOLUTION 1 - Return a single longest word
-  // SOLUTION 2 - Return an array and include multiple words if they have the same length
+  /*
+  let ans = ''
+  let ansCount = 0
+  for(word of sen.split(" "))
+  {
+    word = word.toLowerCase()
+    for(char of word){
+      if(char < 'a' || char > 'z')
+        word = word.replace(char, "")
+    }
+    if(word.length > ansCount){
+      ans = word
+      ansCount = word.length
+    }
+  }
+  return ans*/
+  // SOLUTION 2 - Return an array and include multiple words if they have the same length:
+  ans = []
+  let ansCount = 0
+  for(word of sen.split(" "))
+  {
+    word = word.toLowerCase()
+    for(char of word){
+      if(char < 'a' || char > 'z')
+        word = word.replace(char, "")
+    }
+    if(word.length >= ansCount){
+      ans.push(word)
+      ansCount = word.length
+    }
+  }
   // SOLUTION 3 - Only return an array if multiple words, otherwise return a string
+  //if(ans.length === 1)
+    //return ans[0]
+  return ans
 }
 
 // CHALLENGE 2: ARRAY CHUNKING
@@ -13,29 +46,88 @@ function longestWord(sen) {
 // ex. chunkArray([1, 2, 3, 4, 5, 6, 7], 3) === [[1, 2, 3],[4, 5, 6],[7]]
 // ex. chunkArray([1, 2, 3, 4, 5, 6, 7], 2) === [[1, 2],[3, 4],[5, 6],[7]]
 
-function chunkArray(arr, len) {}
+function chunkArray(arr, len) {
+  let ans = []
+  let temp = []
+  for(i = 0; i < arr.length; i++){
+    temp.push(arr[i])
+    if(i%len === (len-1) || i === (arr.length-1)){
+      ans.push(temp)
+      temp = []
+    }
+  }
+  return ans
+}
 
 // CHALLENGE 3: FLATTEN ARRAY
 // Take an array of arrays and flatten to a single array
 // ex. [[1, 2], [3, 4], [5, 6], [7]] = [1, 2, 3, 4, 5, 6, 7]
 
-function flattenArray(arrays) {}
+function flattenArray(arrays) {
+  ans=[]
+  for(array of arrays){
+    if(Array.isArray(array)){
+      for(element of array){
+        ans.push(element)
+    }
+    
+    }
+    else 
+      return arrays
+  }
+  return ans
+}
 
 // CHALLENGE 4: ANAGRAM
 // Return true if anagram and false if not
 // ex. 'elbow' === 'below'
 // ex. 'Dormitory' === 'dirty room##'
 
-function isAnagram(str1, str2) {}
+function removeNonCharacters(word){
+  word = word.toLowerCase()
+    for(char of word){
+      if(char < 'a' || char > 'z')
+        word = word.replace(char, "")
+    }
+  return word;
+}
+
+function isAnagram(str1, str2) {
+  str1 = removeNonCharacters(str1)
+  str2 = removeNonCharacters(str2)
+  arr1 = str1.split("")
+  arr2 = str2.split("")
+  if(arr1.sort().join()===arr2.sort().join())
+    return true
+
+  return false  
+}
+
+
 
 // CHALLENGE 5: LETTER CHANGES
 // Change every letter of the string to the one that follows it and capitalize the vowels
 // Z should turn to A
 // ex. 'hello there' === 'Ifmmp UIfsf'
 
-function letterChanges(str) {}
+function letterChanges(str) {
+  temp = str.split("")
+  for(i=0; i<temp.length; i++){
+    char=str[i]
+    if((char >="a" && char <="z") || (char >='A' && char <='Z')){
+      if(char === 'z')
+        char = 'a'
+      else if(char ==='Z')
+        char = "A"
+      else
+      char = String.fromCharCode(char.charCodeAt(0)+1)
+    }
+    temp[i]=char
+  }
+  return temp.join("")
+}
 
 // Call Function
-const output = longestWord('Hello, my name is Brad');
+const output = isAnagram("q  we    234", "e  453453 wq");
 
 console.log(output);
